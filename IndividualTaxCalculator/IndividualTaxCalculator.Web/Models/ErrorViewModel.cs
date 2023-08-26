23ae63485217484e11f1cc31332cfef4476b0874
Jared Benade <jared.benade@gmail.com>
2023-08-26 15:46:44 +0200

    Bootstrap project

---
+++ IndividualTaxCalculator/IndividualTaxCalculator.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace IndividualTaxCalculator.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
