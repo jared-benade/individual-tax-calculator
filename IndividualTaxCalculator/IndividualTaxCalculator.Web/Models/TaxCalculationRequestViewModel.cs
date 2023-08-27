d80b5bb1913a67bb70560aa50791022aa40457c9
Jared Benade <jared.benade@gmail.com>
2023-08-27 20:20:03 +0200

    Add select list for postal code

--- IndividualTaxCalculator/IndividualTaxCalculator.Web/Models/TaxCalculationRequestViewModel.cs
+++ IndividualTaxCalculator/IndividualTaxCalculator.Web/Models/TaxCalculationRequestViewModel.cs
@@ -1,10 +1,20 @@
 ﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace IndividualTaxCalculator.Web.Models;
 
 public class TaxCalculationRequestViewModel
 {
     [DisplayName("Postal Code")] public string PostalCode { get; set; } = "";
+
+    // TODO: Populate postal codes from possible choices in DB
+    public IEnumerable<SelectListItem> PostalCodes => new List<SelectListItem>
+    {
+        new() { Value = "7441", Text = "7441" },
+        new() { Value = "A100", Text = "A100" },
+        new() { Value = "7000", Text = "7000" },
+        new() { Value = "1000", Text = "1000" },
+    };
 
     [DisplayName("Annual Income")] public decimal AnnualIncome { get; set; }
 }
